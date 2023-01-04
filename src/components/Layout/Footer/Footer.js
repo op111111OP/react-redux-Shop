a3ea085825e47ba60f039644e642df401eb157b9
Igo467 <www.2017asdlsop@gmail.com>
2023-01-04 20:24:22 +0200

    Main

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -2,7 +2,11 @@ import React from 'react'
 import "./Footer.css"
 function Footer() {
   return (
-    <div>Footer</div>
+    <ul className='number'>
+      <li>+380 991 3049</li>
+      <li>+380 931 4069</li>
+      <li>+380 935 3789</li>
+    </ul>
   )
 }
 
