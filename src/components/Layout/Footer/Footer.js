413afd053fe687461fdd99cd2c0079a880e5a8ca
Igo467 <www.2017asdlsop@gmail.com>
2023-01-06 20:06:49 +0200

    Main

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import "./Footer.css"
+
+// Layout
+
 function Footer() {
   return (
-    <ul className='number'>
-      <li>+380 991 3049</li>
-      <li>+380 931 4069</li>
-      <li>+380 935 3789</li>
-    </ul>
+    <div className='number'>
+      <p>+380 991 3049</p>
+      <p>+380 931 4069</p>
+      <p>+380 935 3789</p>
+    </div>
   )
 }
 
