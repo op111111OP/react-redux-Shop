8d727dc4e058032cf93b336113af75324ec3e592
Igo467 <www.2017asdlsop@gmail.com>
2023-01-21 11:41:30 +0200

    Main

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -1,17 +1,11 @@
-<<<<<<< HEAD
 import React from 'react'
 import { Instagram } from 'react-bootstrap-icons'
 import "./Footer.css"
-=======
-import React from "react";
-import "./Footer.css";
->>>>>>> e8c9a5422e935f58b6c08e5d7f058f4a82b6b9f1
 
 // Layout
 
 function Footer() {
   return (
-<<<<<<< HEAD
  <>
     <div className='icon'>  
       <Instagram size={50} color="blueviolet" />
@@ -24,16 +18,6 @@ function Footer() {
     </div>
     </>
   )
-=======
-    <div className="footer_num">
-      <div className="number">
-        <p>+380 991 3049</p>
-        <p>+380 931 4069</p>
-        <p>+380 935 3789</p>
-      </div>
-    </div>
-  );
->>>>>>> e8c9a5422e935f58b6c08e5d7f058f4a82b6b9f1
 }
 
-export default Footer;
+export default Footer
