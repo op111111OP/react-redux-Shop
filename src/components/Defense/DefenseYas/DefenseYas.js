268be959879fd4edf8a1662e51e8fb7ff7932794
op111111OP <p47601758@gmail.com>
2023-01-22 09:02:49 +0200

    forst

--- src/components/Defense/DefenseYas/DefenseYas.js
+++ src/components/Defense/DefenseYas/DefenseYas.js
@@ -1,12 +1,58 @@
 import React, { useContext } from "react";
+import { XCircle } from "react-bootstrap-icons";
 import Context from "../../../common/context";
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
 
 // App
 function DefenseYas() {
   const { defenseCard } = useContext(Context);
+  return (
+    <div className="container card_box">
+      <div className="container_cards">
+        {defenseCard.map((item) => (
+          <div key={item.id} className="card_conteiner">
+            <Link className="link link_card" to={`/post/${item.id}`}>
+              <div className="card_text">товар</div>
+              <div className="goods_card">
+                <div className="card_conteiner_img">
+                  <img src={item.image} className="card_img" />
+                </div>
+                <div className="card_title"> {item.title}</div>
+              </div>
+            </Link>
+            <div className="card_conteiner_numbers">
+              <div className="card_conteiner_text">
+                <div>кількість</div>
+                <div>ціна</div>
+                <div>сума</div>
+              </div>
+              <div className="card_numbers">
+                <div> {item.price}грн.</div>
 
-  console.log(defenseCard);
-  return <div>dddddddddddd</div>;
+                <div className="card_but_conteiner">
+                  <Button
+                    className="electronics_but card_but"
+                    size="sm"
+                    variant="success"
+                  >
+                    Оформити замовлення
+                  </Button>
+                  <div>
+                    <XCircle
+                      color="grin"
+                      size={21}
+                      className="cart cart_cirle"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default DefenseYas;
