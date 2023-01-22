a0367456a0c7332ef0e72c90c4f981043f7f2b16
op111111OP <p47601758@gmail.com>
2023-01-22 09:29:52 +0200

    forst

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -1,23 +1,23 @@
-import React from 'react'
-import { Instagram } from 'react-bootstrap-icons'
-import "./Footer.css"
+import React from "react";
+import { Instagram } from "react-bootstrap-icons";
+import "./Footer.css";
 
 // Layout
 
 function Footer() {
   return (
- <>
-    <div className='icon'>  
-      <Instagram size={50} color="blueviolet" />
-    </div>
+    <div className="footer container">
+      <div className="icon">
+        <Instagram size={50} color="blueviolet" />
+      </div>
 
-    <div className='number'>
-      <p>+380 991 3049</p>
-      <p>+380 931 4069</p>
-      <p>+380 935 3789</p>
+      <div className="number">
+        <p>+380 991 3049</p>
+        <p>+380 931 4069</p>
+        <p>+380 935 3789</p>
+      </div>
     </div>
-    </>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
